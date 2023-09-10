0ebe6e816c363458a173d3e84809f2cc74180824
uriel197 <60386860+uriel197@users.noreply.github.com>
2023-09-10 13:15:16 +0300

    Update settings.js

--- js files/settings.js
+++ js files/settings.js
@@ -2,6 +2,30 @@
 const rails = document.querySelectorAll('.btn-rail');
 const buttons = document.querySelectorAll('.rail-btn');
 
+function toggleDarkMode() {    
+    if (retrieveFromLocalStorage.textContent === 'Dark Mode') {
+        retrieveFromLocalStorage.textContent = 'Light Mode';
+    } else {
+        retrieveFromLocalStorage.textContent = 'Dark Mode';
+    }
+    body.classList.toggle('dark-mode');  /* 1 */
+    setUserPreference('color-mode', body.classList.contains('dark-mode') ? 'dark' : 'light');
+    setUserPreference('color-mode-text', body.classList.contains('dark-mode') ? 'Light Mode' : 'Dark Mode');
+}
+
+
+function fontSizeToggle() {
+    const isIncreased = JSON.parse(localStorage.getItem('font-size-increased')) || false;
+    if(!isIncreased) {
+        const currentFontSize = parseFloat(getComputedStyle(root).fontSize);
+        const newFontSize = currentFontSize + incrementAmount;
+        root.style.fontSize = `${newFontSize}px`;
+    } else {
+        root.style.fontSize = "";
+    }
+    localStorage.setItem('font-size-increased', JSON.stringify(!isIncreased));
+}
+
 function buttonToggle(button, rail) {
     button.classList.toggle('btn-on'); // Toggle between on and off classes
     rail.classList.toggle('rail-on');
@@ -12,12 +36,7 @@ buttons.forEach((button, index) => {
     button.addEventListener('click', () => {
         const rail = rails[index];
         buttonToggle(button, rail); 
-        if(index === 0) {    
-            if (retrieveFromLocalStorage.textContent === 'Dark Mode') {
-                retrieveFromLocalStorage.textContent = 'Light Mode';
-            } else {
-                retrieveFromLocalStorage.textContent = 'Dark Mode';
-            }
+        if(index === 0) {
             toggleDarkMode();
         }             
     });
@@ -25,12 +44,7 @@ buttons.forEach((button, index) => {
 
 buttons.forEach((button, index) => {
     button.addEventListener('keydown', (e) => {
-        if(e.key === 'Enter' && index === 0) { 
-            if (retrieveFromLocalStorage.textContent === 'Dark Mode') {
-                retrieveFromLocalStorage.textContent = 'Light Mode';
-            } else {
-                retrieveFromLocalStorage.textContent = 'Dark Mode';
-            }
+        if(e.key === 'Enter' && index === 0) {
             toggleDarkMode();
         }
     })
@@ -43,16 +57,7 @@ const root = document.documentElement; // Get the <html> element
 buttons.forEach((button, index) => {
     button.addEventListener('click', () => {
         if(index === 1) {
-            const isIncreased = JSON.parse(localStorage.getItem('font-size-increased')) || false;
-
-            if(!isIncreased) {
-                const currentFontSize = parseFloat(getComputedStyle(root).fontSize);
-                const newFontSize = currentFontSize + incrementAmount;
-                root.style.fontSize = `${newFontSize}px`;
-            } else {
-                root.style.fontSize = null
-            }
-            localStorage.setItem('font-size-increased', JSON.stringify(!isIncreased));
+            fontSizeToggle();
         }
     })
 });
@@ -60,22 +65,10 @@ buttons.forEach((button, index) => {
 buttons.forEach((button, index) => {
     button.addEventListener('keydown', function(e) {
         if(e.key === 'Enter' && index === 1) {
-            const isIncreased = JSON.parse(localStorage.getItem('font-size-increased')) || false;
-
-            if(!isIncreased) {
-                const currentFontSize = parseFloat(getComputedStyle(root).fontSize);
-                const newFontSize = currentFontSize + incrementAmount;
-                root.style.fontSize = `${newFontSize}px`;
-            } else {
-                root.style.fontSize = null
-            }
-            localStorage.setItem('font-size-increased', JSON.stringify(!isIncreased));
+            fontSizeToggle();
         }
     })
 });
-
-
-
 
 
 
